7343c6722c9104ec2269655e17232e4e87beafb9
Olivia Auzenne <oauzenne@gmail.com>
2019-05-13 00:45:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { Provider } from 'react-redux';
 import manageUsers from './reducers/manageUsers'
 
 
-// add imports and code
+const store = createStore(
+  manageUsers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 ReactDOM.render(
